4abba325b19bd42d1ea7b9d085f7304b25a99e4c
Marc Dangschat <marc.dangschat@gmail.com>
2018-04-13 16:04:21 +0200

    Reduced max sentence output for decoded.
    closes #26

--- python/s_utils.py
+++ python/s_utils.py
@@ -5,7 +5,7 @@ from git import Repo
 import tensorflow as tf
 
 from s_labels import itoc
-from s_params import NP_FLOAT
+from s_params import FLAGS, NP_FLOAT
 
 
 class AdamOptimizerLogger(tf.train.AdamOptimizer):
@@ -189,8 +189,9 @@ def dense_to_text(decoded, originals):
     for o in originals:
         original_strings.append(''.join([c for c in o.decode('utf-8')]))
 
-    # L8ER: Remove debug print.
-    print('d: {}\no: {}'.format(decoded_strings, original_strings))
+    # Print a maximum of `FLAGS.num_samples_to_report` to STDOUT.
+    print('d: {}\no: {}'.format(decoded_strings[: FLAGS.num_samples_to_report],
+                                original_strings[: FLAGS.num_samples_to_report]))
 
     decoded_strings = np.array(decoded_strings, dtype=np.object)
     original_strings = np.array(original_strings, dtype=np.object)
