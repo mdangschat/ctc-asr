e81a2d67789d581ba7445ea71a4560107c82c471
Marc Dangschat <marc.dangschat@gmail.com>
2018-05-02 08:58:37 +0200

    LR

--- python/params.py
+++ python/params.py
@@ -14,11 +14,11 @@ tf.flags.DEFINE_integer('batch_size', 4,
                         """(Maximum) Number of samples within a batch.""")
 
 # Learning rate.
-tf.flags.DEFINE_float('learning_rate', 1e-4,
+tf.flags.DEFINE_float('learning_rate', 1e-3,
                       """Initial learning rate.""")
-tf.flags.DEFINE_float('learning_rate_decay_factor', 1/5,
+tf.flags.DEFINE_float('learning_rate_decay_factor', 2/3,
                       """Learning rate decay factor.""")
-tf.flags.DEFINE_integer('steps_per_decay', 10000,
+tf.flags.DEFINE_integer('steps_per_decay', 20000,
                         """Number of epochs after which learning rate decays.""")
 
 # Optimizer.
