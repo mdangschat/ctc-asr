798642114dac214ff65790a3d508ab58b988fb43
Marc Dangschat <marc.dangschat@gmail.com>
2018-05-14 11:16:14 +0200

    Training directory
    ref #31

--- python/params.py
+++ python/params.py
@@ -7,7 +7,7 @@ from python.s_labels import num_classes
 
 
 # Constants describing the training process.
-tf.flags.DEFINE_string('train_dir', '/home/marc/workspace/speech_checkpoints/ds_lstm_3',
+tf.flags.DEFINE_string('train_dir', '/home/marc/workspace/speech_checkpoints/ds_1',
                        """Directory where to write event logs and checkpoints.""")
 
 tf.flags.DEFINE_integer('batch_size', 4,
