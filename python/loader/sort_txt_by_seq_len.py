71228545f231d0b84dc69ad325497772688ca43c
Marc Dangschat <marc.dangschat@gmail.com>
2018-05-16 19:24:19 +0200

    Added method to sort a .txt file by sequence length
    References #81

---
+++ python/loader/sort_txt_by_seq_len.py
@@ -0,0 +1,63 @@
+"""
+Sort a train.txt like file by it's audio files sequence length.
+"""
+
+import os
+import sys
+
+from tqdm import tqdm
+
+from utils import storage
+from python.loader.load_sample import wav_length
+
+
+DATASETS_PATH = '/home/marc/workspace/datasets/speech_data'
+
+
+def _sort_txt_by_seq_len(txt_path):
+    """Sort a train.txt like file by it's audio files sequence length.
+
+    Args:
+        txt_path (str): Path to the `train.txt`.
+
+    Returns:
+        Nothing.
+    """
+
+    # Read train.txt file.
+    with open(txt_path, 'r') as f:
+        lines = f.readlines()
+        read_length = len(lines)
+
+        buffer = []   # Output buffer.
+
+        for line in tqdm(lines, desc='Reading audio samples', total=len(lines), file=sys.stdout,
+                         unit='samples', dynamic_ncols=True):
+            wav_path, label = line.split(' ', 1)
+
+            length = int(wav_length(os.path.join(DATASETS_PATH, wav_path)))
+            buffer.append((length, wav_path, label))
+
+        # Sort.
+        buffer = sorted(buffer, key=lambda x: x[0])
+
+        # Remove seq_length.
+        buffer = ['{} {}'.format(p, l) for _, p, l in buffer]
+
+    # Write back to file.
+    assert read_length == len(buffer)
+    storage.delete_file_if_exists(txt_path)
+    with open(txt_path, 'w') as f:
+        f.writelines(buffer)
+
+    with open(txt_path, 'r') as f:
+        assert len(f.readlines()) == read_length, \
+            'Something went wrong writing the data back to file.'
+
+
+if __name__ == '__main__':
+    # Path to `train.txt` file.
+    _txt_path = os.path.join('/home/marc/workspace/speech/data', 'train.txt')
+
+    # Display dataset stats.
+    _sort_txt_by_seq_len(_txt_path)
