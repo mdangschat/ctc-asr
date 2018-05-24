0c59412711022f4b40c36ccee0908545e912c3fe
Marc Dangschat <marc.dangschat@gmail.com>
2018-05-24 15:52:34 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='asr',
     version='v0.0.3',
-    packages=['', 'util', 'loader'],
+    packages=['', 'asr'],
     package_dir={'': 'asr'},
-    url='https://vcs.zwuenf.org/mdangschat/asr',
+    url='https://vcs.zwuenf.org/mdangschat/speech',
     license='',
     author='Marc Dangschat',
     author_email='dangschat@fh-muenster.de',
-    description='TensorFlow asr recognition system.'
+    description='TensorFlow based Automatic Speech Recognition System.'
 )
