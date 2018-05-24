3eac6785293c0885ee05a5122cd6df640cf579f3
Marc Dangschat <marc.dangschat@gmail.com>
2018-05-24 15:24:02 +0200

    Added setup.py

---
+++ python/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='speech',
+    version='v0.0.3',
+    packages=['', 'util', 'loader'],
+    package_dir={'': 'python'},
+    url='https://vcs.zwuenf.org/mdangschat/speech',
+    license='',
+    author='Marc Dangschat',
+    author_email='dangschat@fh-muenster.de',
+    description='TensorFlow speech recognition system.'
+)
