e53704bccad6d8dcc9c9ea37b8014b35d4669b10
Marc Dangschat <marc.dangschat@gmail.com>
2018-06-10 22:16:43 +0200

    Updated dataset sizes #128, #130

--- asr/params.py
+++ asr/params.py
@@ -83,9 +83,9 @@ tf.flags.DEFINE_bool('features_drop_every_second_frame', False,
 # Dataset.
 tf.flags.DEFINE_integer('sampling_rate', 16000,
                         "The sampling rate of the audio files (2 * 8kHz).")
-tf.flags.DEFINE_integer('num_examples_train', 272537,
+tf.flags.DEFINE_integer('num_examples_train', 570351,
                         "Number of examples in the training set. `test.txt`")
-tf.flags.DEFINE_integer('num_examples_test', 3679,
+tf.flags.DEFINE_integer('num_examples_test', 3690,
                         "Number of examples in the testing/evaluation set. `test.txt`")
 tf.flags.DEFINE_integer('num_examples_dev', 2703,
                         "Number of examples in the validation set. `dev.txt`")
