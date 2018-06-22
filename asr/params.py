969094b48a5917383d51e8e2fe751e40c2069bf1
Marc Dangschat <marc.dangschat@gmail.com>
2018-06-22 09:10:36 +0200

    Dense vs. conv layers comparison run configuration

--- asr/params.py
+++ asr/params.py
@@ -7,7 +7,7 @@ from asr.s_labels import num_classes
 
 
 # Constants describing the training process.
-tf.flags.DEFINE_string('train_dir', '../speech_checkpoints/3c3r2d_1',
+tf.flags.DEFINE_string('train_dir', '../speech_checkpoints/3c1r2d_mel_local',
                        "Directory where to write event logs and checkpoints.")
 tf.flags.DEFINE_integer('batch_size', 8,
                         "Number of samples within a batch.")
@@ -59,7 +59,7 @@ tf.flags.DEFINE_string('used_model', 'ds2',
 tf.flags.DEFINE_multi_integer('conv_filters', [32, 32, 96],
                               "Number of filters for each convolutional layer.")
 
-tf.flags.DEFINE_integer('num_layers_rnn', 3,
+tf.flags.DEFINE_integer('num_layers_rnn', 1,
                         "Number of stacked RNN cells.")
 tf.flags.DEFINE_integer('num_units_rnn', 2048,
                         "Number of hidden units in each of the RNN cells.")
@@ -77,7 +77,7 @@ tf.flags.DEFINE_string('feature_normalization', 'local',
                        ("Type of normalization applied to input features."
                         "Supported are: 'none', 'global', 'local', and 'local_scalar'"))
 tf.flags.DEFINE_bool('features_drop_every_second_frame', False,
-                     "[Deep Speech 1] like dropping of every 2nd input time frame.")
+                     "[Deep Speech 1] style, dropping of every 2nd input time frame.")
 
 # Dataset.
 tf.flags.DEFINE_integer('sampling_rate', 16000,
