60fb3d5569bb3f35c1e13c40af9e40e274d538ad
Marc Dangschat <marc.dangschat@gmail.com>
2018-07-08 17:00:53 +0200

    Patch for too long(short) examples #137
    Tatoeba files buggy, possible fix for wavfile read
    Fixed libri speech loader bug. Validation with wrong wav path.
    Fixed timit loader bug. Validation with wrong wav path.
    Circular include fixed.
    RC6.1 Testing needed!

--- asr/util/storage.py
+++ asr/util/storage.py
@@ -54,6 +54,7 @@ def delete_file_if_exists(path):
         for i in range(5):
             try:
                 os.remove(path)
+                continue
             except (OSError, ValueError) as e:
                 print('TODO CAUGHT error at file: ', path)
                 print('ERROR=', e)
