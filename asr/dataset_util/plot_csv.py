7ce5c592637dcb45acc6a5cae89ce7af1ccad147
Marc Dangschat <marc.dangschat@gmail.com>
2018-07-20 15:26:13 +0200

    Added csv plotter prototype

---
+++ asr/dataset_util/plot_csv.py
@@ -0,0 +1,18 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+CSV_FILE = '/home/marc/Downloads/test.csv'
+
+data = np.loadtxt(CSV_FILE, skiprows=1, delimiter=',')
+
+data = data[:, 1:]
+print('data:', data)
+
+plt.figure()
+plt.plot(data[:, [0]], data[:, [1]], linestyle='-', linewidth=0.9, color='#1111EE', marker='o')
+plt.grid(visible=True)
+plt.ylabel('WER')
+plt.xlabel('Step')
+plt.title('AIDS', visible=False)
+plt.show()
