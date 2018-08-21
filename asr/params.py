b84831fef563a2d1ec841e962dc1df73afee493f
Marc Dangschat <marc.dangschat@gmail.com>
2018-08-21 14:02:34 +0200

    Runconfig for COSY

--- asr/params.py
+++ asr/params.py
@@ -9,9 +9,9 @@ from asr.labels import num_classes
 
 
 # Constants describing the training process.
-tf.flags.DEFINE_string('train_dir', '../speech_checkpoints/3c5r2d_mfcc_local_lstm_2',
+tf.flags.DEFINE_string('train_dir', '../speech_checkpoints/3c1r2d_mfcc_localscalar',
                        "Directory where to write event logs and checkpoints.")
-tf.flags.DEFINE_integer('batch_size', 16,
+tf.flags.DEFINE_integer('batch_size', 8,
                         "Number of samples within a batch.")
 
 
@@ -69,7 +69,7 @@ tf.flags.DEFINE_string('used_model', 'ds2',
 tf.flags.DEFINE_multi_integer('conv_filters', [32, 32, 96],
                               "Number of filters for each convolutional layer.")
 
-tf.flags.DEFINE_integer('num_layers_rnn', 5,
+tf.flags.DEFINE_integer('num_layers_rnn', 1,
                         "Number of stacked RNN cells.")
 tf.flags.DEFINE_integer('num_units_rnn', 2048,
                         "Number of hidden units in each of the RNN cells.")
@@ -84,7 +84,7 @@ tf.flags.DEFINE_float('relu_cutoff', 20.0,
 # Input features.
 tf.flags.DEFINE_string('feature_type', 'mfcc',
                        "Type of input features. Supported types are: 'mel' and 'mfcc'.")
-tf.flags.DEFINE_string('feature_normalization', 'local',
+tf.flags.DEFINE_string('feature_normalization', 'local_scalar',
                        ("Type of normalization applied to input features."
                         "Supported are: 'none', 'global', 'local', and 'local_scalar'"))
 tf.flags.DEFINE_boolean('features_drop_every_second_frame', False,
